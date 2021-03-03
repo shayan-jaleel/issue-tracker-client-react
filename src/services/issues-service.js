a5499ab9f6877c1c507988eaf6e0444290146f56
shayan_jaleel <shayan.jaleel@gmail.com>
2021-03-02 23:00:09 -0500

    Add issue creation service and use it for the create-issue component.

--- src/services/issues-service.js
+++ src/services/issues-service.js
@@ -4,9 +4,19 @@ const findAllIssues = () =>
     fetch(ISSUES_URL)
         .then(response => response.json())
 
+const createIssue = (issue) =>
+    fetch(ISSUES_URL, {
+        method: 'POST',
+        body: JSON.stringify(issue),
+        headers: {
+            'content-type' : 'application/json'
+        }
+    }).then(response => response.json())
+
 
 const IssuesService = {
-    findAllIssues
+    findAllIssues,
+    createIssue
 }
 
 export default IssuesService
