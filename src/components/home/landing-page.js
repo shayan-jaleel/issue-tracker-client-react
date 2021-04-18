ad653586162f42de50885e1e24e8ee3c0d6d8828
shayan_jaleel <shayan.jaleel@gmail.com>
2021-04-17 23:40:15 -0400

    Font styling changes to landing page and sidebar.

--- src/components/home/landing-page.js
+++ src/components/home/landing-page.js
@@ -3,11 +3,11 @@ import {Link} from "react-router-dom";
 const LandingPage = () =>
     <div className="container-fluid">
         <h1 className="header text-center font-weight-bold m-2">Issue Tracker - Track Bugs and Feature Requests</h1>
-        <h3 className="header text-center text-secondary m-2">Get a clear centralized overview of development requests.</h3>
+        <h3 className="header text-center font-weight-bold text-secondary m-2">Get a clear centralized overview of development requests.</h3>
         <h5 className="header text-center text-info m-2">Track your progress. Sign up below!</h5>
         <div className="d-lg-flex justify-content-center">
-            <Link to="/login" className="btn-primary btn-lg btn p-5 m-5">Login</Link>
-            <Link to="/signup" className="btn-success btn-lg btn p-5 m-5">Signup</Link>
+            <Link to="/login" className="btn-primary btn-lg btn p-5 m-5 font-weight-bold">Login</Link>
+            <Link to="/signup" className="btn-success btn-lg btn p-5 m-5 font-weight-bold">Signup</Link>
         </div>
     </div>
 
--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,12 +4,12 @@ const Sidebar = () =>
     <div>
         <h4>
             <ul className="list-group">
-                <li className="list-group-item"><Link to="/user-roles">Manage Roles</Link></li>
-                <li className="list-group-item"><Link to="/projects">My Projects</Link></li>
-                <li className="list-group-item"><Link to="/issues">My Issues</Link></li>
-                <li className="list-group-item"><Link to="/login">Login</Link></li>
-                <li className="list-group-item"><Link to="/profile">My Profile</Link></li>
-                <li className="list-group-item"><Link to="/landing">Landing Page</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/user-roles">Manage Roles</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/projects">My Projects</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/issues">My Issues</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/login">Login</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/profile">My Profile</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/landing">Landing Page</Link></li>
             </ul>
         </h4>
     </div>
