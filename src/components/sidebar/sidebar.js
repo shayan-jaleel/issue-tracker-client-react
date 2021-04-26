e10d46345f9e6d46ea6e370caea9ee9e298e6ade
shayan_jaleel <shayan.jaleel@gmail.com>
2021-04-26 18:28:23 -0400

    Hide sidebar for small screens.

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -4,16 +4,18 @@ import {connect} from "react-redux";
 const Sidebar = ({userLoggedIn}) =>
     <div>
         <h4>
+            <span className="d-none d-md-block">
             <ul className="list-group">
                 {userLoggedIn && userLoggedIn.role.name === 'ADMIN' &&
-                <li className="list-group-item font-weight-bold"><Link to="/user-roles">Manage Users</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/user-roles" className="red-link">Manage Users</Link></li>
                 }
-                <li className="list-group-item font-weight-bold"><Link to="/projects">My Projects</Link></li>
-                <li className="list-group-item font-weight-bold"><Link to="/issues">My Issues</Link></li>
-                <li className="list-group-item font-weight-bold"><Link to="/login">Login</Link></li>
-                <li className="list-group-item font-weight-bold"><Link to="/profile">My Profile</Link></li>
-                <li className="list-group-item font-weight-bold"><Link to="/landing">Landing Page</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/projects" className="red-link">My Projects</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/issues" className="red-link">My Issues</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/login" className="red-link">Login</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/profile" className="red-link">My Profile</Link></li>
+                <li className="list-group-item font-weight-bold"><Link to="/landing" className="red-link">Landing Page</Link></li>
             </ul>
+            </span>
         </h4>
     </div>
 
