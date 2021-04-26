6b95a6bd88ec38154a46f890b6949666d6e86b02
shayan_jaleel <shayan.jaleel@gmail.com>
2021-04-26 15:17:05 -0400

    Landing page styling change.

--- src/components/home/landing-page.js
+++ src/components/home/landing-page.js
@@ -2,9 +2,13 @@ import {Link} from "react-router-dom";
 
 const LandingPage = () =>
     <div className="container-fluid">
-        <h1 className="header text-center font-weight-bold m-2">Issue Tracker - Track Bugs and Feature Requests</h1>
+        <h1 className="header text-center font-weight-bold m-2">
+            <i className="fas text-danger fa-tasks"/><span className="ml-2">On</span>
+                <span className="text-danger font-weight-bold">Track </span>
+            - Track Bugs and Feature Requests
+        </h1>
         <h3 className="header text-center font-weight-bold text-secondary m-2">Get a clear centralized overview of development requests.</h3>
-        <h5 className="header text-center text-info m-2">Track your progress. Sign up below!</h5>
+        <h5 className="header text-center text-danger m-2">Track your progress. Sign up below!</h5>
         <div className="d-lg-flex justify-content-center">
             <Link to="/login" className="btn-primary btn-lg btn p-5 m-5 font-weight-bold">Login</Link>
             <Link to="/signup" className="btn-success btn-lg btn p-5 m-5 font-weight-bold">Signup</Link>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,7 +2,8 @@ const Navbar = () =>
     <div className="m-3">
         <div className="row">
             <i className="fas text-danger fa-tasks fa-2x ml-3"/>
-            <h3 className="col-4 font-weight-bold">On<span className="text-danger font-weight-bold">Track</span><span className="text-secondary"></span></h3>
+            <h3 className="col-4 font-weight-bold">On
+                <span className="text-danger font-weight-bold">Track</span></h3>
             <h3 className="col-5"> <div className="float-right col-3 mr-2">Welcome</div>
                 {/*<div className="float-right mr-5"></div>*/}
                 {/*<div className="float-right mr-5">*/}
